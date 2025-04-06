6132a76523b8629ee37264042cf2c5b741d319e4
pea@misfist.com <pea@PEA-Lutzs-Mac-Mini.local>
2025-04-05 23:34:08 -0400

    Add article details block

--- src/app/blocks/build/book-details/index.asset.php
+++ src/app/blocks/build/book-details/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-editor', 'wp-i18n'), 'version' => '34a22ae3caa4c99845d8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-editor', 'wp-i18n'), 'version' => '70396045a90c113c9b87');
